ef89fb95cbd362ecff9e3e19b9fd4e3bf0841012
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2022-01-03 00:29:31 +0200

    homepage-edit-3

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 export default function Header() {
     return (
         <div className='header'>            
-     <h1> BookMe</h1>
+    <a href='/'> <h1> BookMe</h1></a>
             <nav>
                 <a href='/'>Home</a>
                 <a href='/'>Cars</a>
