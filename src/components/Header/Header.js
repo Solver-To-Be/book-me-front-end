63e70ed08a908a2619b001541f96e932f7abf096
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2022-01-04 15:12:32 +0200

    edit-9

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,44 @@
-import React, { useContext } from 'react';
-import { LoginContext } from '../signUp/Auth';
+import React, { Component } from 'react'
 import './Header.css'
 
-export default function Header() {
-    const context = useContext(LoginContext);
-    return (
-        <div className='header'>            
-    <a href='/'> <h1> BookMe</h1></a>
-            <nav>
-                <a href='/'>Home</a>
-                <a href='/cars'>Cars</a>
-                <a href='/myAcount'>My Acount</a>
-                <a href='/' onClick={context.logout}>Log Out</a>
-            </nav>
-          
-        </div>
-    )
+
+export class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scrolling: false,
+
+        };
+    }
+
+    changeBackroung = () => {
+        window.scrollY > 0
+            ? this.setState({
+                scrolling: true,
+            })
+            : this.setState({
+                scrolling: false,
+            });};
+
+        componentDidMount = () => {
+            window.addEventListener("scroll", this.changeBackroung);
+        };
+    
+    render() {
+        return (
+            <div className={this.state.scrolling ? "header_active" : "header"}>
+                <a href='/'> <h1> BookMe</h1></a>
+                <nav>
+                    <a href='/'>Home</a>
+                    <a href='/cars'>Cars</a>
+                    <a href='/myAcount'>My Acount</a>
+                    <a href='/'>Log Out</a>
+                </nav>
+
+            </div>
+        )
+    }
 }
+
+export default Header
+
